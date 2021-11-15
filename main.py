942a539dead50899dd96f9efa60a919481ff13e6
Prakhar Trivedi <prakh0706@outlook.com>
2021-11-15 16:37:07 +0800

    Initiat commit v2

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+def fileContent(fileName):
+    with open(fileName, 'r') as f:
+        f_content = f.read()
+        return f_content
+
+@app.route('/')
+def home():
+    return fileContent('homepage.html')
+
+@app.route('/newUpdate', methods=['POST'])
+def newUpdate():
+    if request.headers['Content-Type'] != 'application/json':
+        return {'failed': "Incorrect Content-Type"}, 400
+    else:
+        for elem in ["monitorID", "monitorURL", "monitorFriendlyName", "alertType", "alertTypeFriendlyName", "alertDuration", "alertDatetime", "monitorAlertContacts"]:
+            if elem not in request.json:
+                return {'failed': "Missing required field: " + elem}, 400
+
+
+# Service assets
+@app.route('/assets/copyright')
+def copyright():
+    return fileContent('copyright.js')
+
+if __name__ == '__main__':
+    app.run(port=8000)
